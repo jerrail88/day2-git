0622e8c784945d4f5f58374a5e9774b82ea57049
Jerrail <jerrail88@gmail.com>
2025-11-20 13:36:47 -0500

    Add Day 3 Exercise 4 - lists and loops

---
+++ day3/day3/day3_4_lists_loops.py
@@ -0,0 +1,12 @@
+# Create a list of fruits
+fruits = ["apple", "banana", "cherry", "date"]
+
+# Loop through the list and print each fruit
+for fruit in fruits:
+    print("I like", fruit)
+
+# Add a new fruit to the list
+fruits.append("dragonfruit")
+print("\nUpdated list:")
+for fruit in fruits:
+    print(fruit)
