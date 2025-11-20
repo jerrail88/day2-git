68387f7cd7e8b6ada086bbbf07ed26819199f158
Jerrail <jerrail88@gmail.com>
2025-11-20 12:54:34 -0500

    Add Day 3 exercises 1 & 2

---
+++ day3/day3/day3_1_print.py
@@ -0,0 +1,3 @@
+print("Hello, Day 3!")
+print("Learning Python step by step.")
+print("Git + Python = progress.")
---
+++ day3/day3/day3_2_input.py
@@ -0,0 +1,7 @@
+# Ask the user for their name and age
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+# Display a message using the variables
+print("Hello, " + name + "!")
+print("You are " + age + " years old.")
