9097052e555ca3766f0a6411bf060945467d299f
Jerrail <jerrail88@gmail.com>
2025-11-20 13:25:29 -0500

    Add Day 3 Exercise 3 - if/else conditions

---
+++ day3/day3/day3_3_conditions.py
@@ -0,0 +1,10 @@
+# Ask user for a number
+number = int(input("Enter a number: "))
+
+# Check if the number is positive, negative, or zero
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
